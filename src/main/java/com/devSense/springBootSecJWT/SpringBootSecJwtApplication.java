08787df4bde75cbe63527b9f74694737801c0a86
coolmp08 <coolmp08@gmail.com>
2024-10-26 01:15:06 +0530

    initial-commit

---
+++ src/main/java/com/devSense/springBootSecJWT/SpringBootSecJwtApplication.java
@@ -0,0 +1,13 @@
+package com.devSense.springBootSecJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devSense/springBootSecJWT/SpringBootSecJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devSense.springBootSecJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/*@SpringBootTest
+class SpringBootSecJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}*/
